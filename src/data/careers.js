6e0075a24ff5465c893b7f474363b8f81dcdec1c
jalannin <lanning.j@husky.neu.edu>
2019-11-30 00:08:55 -0500

    Career Data

--- src/data/careers.js
+++ src/data/careers.js
@@ -6,4 +6,25 @@ export default [
         education: "Masters/PhD",
         experience: "2-3 years",
     },
+    {
+        career: "Software Engineer",
+        description: "Software Engineers work in code",
+        salary: "10000",
+        education: "Bachelors",
+        experience: "0-3 years",
+    },
+    {
+        career: "Teaching",
+        description: "We always need teachers",
+        salary: "50000",
+        education: "Bachelors",
+        experience: "3-5 years",
+    },
+    {
+        career: "Doctor",
+        description: "Good luck, gg",
+        salary: "300000",
+        education: "MD/PhD",
+        experience: "5-8 years",
+    },
 ]
