e240b0afbfc846626a467a84ce3f052ec714b3a2
jalannin <lanning.j@husky.neu.edu>
2019-11-29 23:12:23 -0500

    bootstrapping the mentor

--- src/components/assets/TutorialCard.vue
+++ src/components/assets/TutorialCard.vue
@@ -3,13 +3,13 @@
         <div class="card">
             <div class="card-image">
                 <div class="embed-responsive embed-responsive-16by9">
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/SC1XE85BC9o" frameborder="0" allowfullscreen></iframe>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/fZIiE92-NkU" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
 
             </div><!-- card image -->
 
             <div class="card-content">
-                <span class="card-title">Will my credits transfer?</span>
+                <span class="card-title">Bill Burr and Segura</span>
 
             </div><!-- card content -->
         </div>
