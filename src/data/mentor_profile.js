de815a80c193c786a8e870a27cb840b7f820b140
sangeethac <chandrashekar.s@husky.neu.edu>
2019-12-03 16:54:04 -0500

    mentor-profile updates

---
+++ src/data/mentor_profile.js
@@ -0,0 +1,48 @@
+export default [
+    {
+        username: 'mindRead',
+        profilePicture: 'https://i0.wp.com/acaweb.org/wp-content/uploads/2018/12/profile-placeholder.png?fit=300%2C300&ssl=1',
+        bio: "As a licensed psychologist in the state of Connecticut with 12+ years of experience\
+        leading individual counseling sessions to help patients achieve their personal goals and objectives, I feel confident that I would help you in achieving your career goal.  I lead\
+        individual counseling sessions for children, adolescents, and adults on an outpatient basis as a Clinical Psychologist for Mission Mental Health; diagnosing mental and emotional disorders, drawing on community resources as needed, and collaborating with patients’ other health providers as necessary to ensure medication compliance.",
+        title: 'Dr. Mind Reader, MD',
+        career: 'Pyschologist',
+        yoe: '12',
+        education: [
+            {
+                school: 'Best University in US',
+                degree: 'MD in Clinial Psychology'  
+            }
+        ],
+        workEx: [{
+                company: 'Can Read You Better',
+                position: 'Chief of Dept.'
+            }],
+        articles: [
+            {
+                title: 'Who Thrives in Confusing Situations',
+                image: 'noImage.png',
+                description: 'New research explores the personality types that respond best to confusion',
+                link: "https://onlinelibrary.wiley.com/doi/full/10.1002/jclp.22892"
+            },
+            {
+                title: 'Mental Health Issues Among Entrepreneurs',
+                image: 'noImage.png',
+                description: 'Multiple reports have come in over the last few years that confirm the fact\
+                 about entrepreneurs being more susceptible to psychiatric problems than their employees.',
+                link: "https://onlinelibrary.wiley.com/doi/full/10.1002/jclp.22904"
+            }
+        ],
+        videos: [
+            {
+                title: 'Introduction to Psychology',
+                link: "https://www.youtube.com/watch?v=vo4pMVb0R6M"
+            },
+            {
+                title: 'Stress in the 21st Century',
+                link: "https://www.youtube.com/watch?v=xp0O2vi8DX4"
+            }
+        ]
+
+    }
+]
