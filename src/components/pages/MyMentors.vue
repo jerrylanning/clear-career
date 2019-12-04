b866d1443cca547c2faa2b2cbbda1971f1dc22f4
sangeethac <chandrashekar.s@husky.neu.edu>
2019-12-04 18:08:14 -0500

    my mentor page

--- src/components/pages/MyMentors.vue
+++ src/components/pages/MyMentors.vue
@@ -1,60 +1,69 @@
 <template>
     <b-container>
-        <h4>My Mentors</h4>
+        <h2>My Mentors</h2>
         <div class="search-mentors"><router-link to="/search-mentors">Search Mentors</router-link></div>
         <b-card-group deck class="mentor-card-container">
-            <div class="mentor-card">
-                <b-card class="mt-3" >
+            <div class="mentor-card" v-for="mentor in mentors" :key="mentor.username">
+                <b-card class="mt-3">
                     <b-card-text>
-                        <h1>My Mentor</h1>
+                        <h4>{{mentor.title}}</h4>
+                        <br>
                         <div class="mentor-populate">
                             <div class="mentor-container">
+                                <b-card title="Articles">
                                 <div class="mentor-articles">
-                                    <ArticleCard/>
-                                    <ArticleCard/>
-
+                                    <ArticleCard v-for="article in mentor.articles" :key="article.title"
+                                             :title="article.title" :link="article.link"
+                                             :img="article.image" :summary="article.description"/>
                                 </div>
+                                </b-card>
+                                <b-card title="Tutorials">
                                 <div class="mentor-tutorials">
-                                    <TutorialCard/>
-                                    <TutorialCard/>
+                                    <TutorialCard v-for="video in mentor.videos" :key="video.link"
+                                    :embedLink="video.link"
+                                    :title="video.title"/><TutorialCard/>
                                 </div>
+                                </b-card>
                             </div>
                         </div>
                     </b-card-text>
                 </b-card>
             </div>
-            <div class="mentor-card">
-                <b-card class="mt-3" >
-                    <b-card-text>
-                        <h1>My Mentor</h1>
-                        <div class="mentor-populate">
-                            <div class="mentor-container">
-                                <div class="mentor-articles">
-                                    <ArticleCard/>
-                                    <ArticleCard/>
 
-                                </div>
-                                <div class="mentor-tutorials">
-                                    <TutorialCard/>
-                                    <TutorialCard/>
-                                </div>
-                            </div>
-                        </div>
-                    </b-card-text>
-                </b-card>
-            </div>
         </b-card-group>
     </b-container>
 </template>
 
 <script>
-    import ArticleCard from "../assets/ArticleCard";
+    import {mapGetters} from 'vuex';
     import TutorialCard from "../assets/TutorialCard";
-
+    import ArticleCard from "../assets/ArticleCard";
     export default {
         name: "MyMentors",
-        components: {
-            TutorialCard, ArticleCard}
+        components: {ArticleCard,TutorialCard},
+        data(){
+            return{
+                mentors: [],
+                user: {}
+            }
+        },
+        computed:{
+            ...mapGetters([
+                'loggedInUser',
+                'getMentorProfileWithUsername',
+                'getUserWithUsername'
+            ]),
+        },
+        methods:{
+
+        },
+        mounted(){
+            this.user = this.getUserWithUsername(this.loggedInUser.username)[0]
+            this.user.mentors.forEach(mentor => {
+                let mentorData = this.getMentorProfileWithUsername(mentor)[0]
+                this.mentors.push(mentorData);
+            });
+        }
     }
 </script>
 
