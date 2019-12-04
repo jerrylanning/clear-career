532c0956fa7bd250aa39f95e3e93edca9abb4cc3
SDM Trainers <trainer@2016-30.fios-router.home>
2019-12-03 19:33:38 -0500

    Added changes to homepage

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -3,13 +3,36 @@
     <div>
         <h1>Home</h1>
         <h4> Hello, {{user.firstName}} </h4>
+            <div class="col">
+                <br>
+                <b-card>
+                    <h2>My Paths</h2>
+                    <hr>
+                    <MyPathsCard/>
+                    <br>
+                    <router-link to="/my-path" tag="button" class="btn btn-primary">Go to My Path</router-link>
+                </b-card>
+            </div>
+            <div class="col">
+                <b-card>
+                <h2>My Mentors</h2>
+                <hr>
+                <MyMentorsCard/>
+                <MyMentorsCard/>
+                <br>
+                <router-link to="/my-mentors" tag="button" class="btn btn-primary">Go to My Mentors</router-link>
+                </b-card>
+            </div>
     </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
+import MyMentorsCard from "../assets/MyMentorsCard";
+import MyPathsCard from "../assets/MyPathsCard";
     export default {
         name: "Home",
+        components: {MyPathsCard, MyMentorsCard},
         data(){
             return {
                  user: {}
@@ -29,5 +52,14 @@ import { mapGetters } from 'vuex'
 </script>
 
 <style scoped>
+
+    .col{
+        width: 40%;
+        display: inline-block;
+    }
+
+    h2{
+        color: dodgerblue;
+    }
 
 </style>
